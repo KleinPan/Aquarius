98bc2c7d10782296ec5186a146c5d05f3a14f9f3
KleinPan <964012840@qq.com>
2020-04-22 09:45:20 +0800

    转存

---
+++ DropYouRem/Model/CofigModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DropYouRem.Model
+{
+    class ConfigModel
+    {
+        public string Command { get; set; }
+
+        public string TargetDirectory { get; set; }
+    }
+}
